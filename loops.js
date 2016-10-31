8442992c3aa06fc7b76b9caab7d964756c2420a7
jason brown <j.grant.brown@gmail.com>
2016-10-31 15:17:30 +0000

    Done.

--- loops.js
+++ loops.js
@@ -2,9 +2,9 @@ var forloop=[];
 
  for (let i=0;i<=25;i++){
   if(i===1){
-  forloop.push("I am 1 strange loop.");
+  forloop=forloop.push("I am 1 strange loop.");
   }
   else{
-    forloop.push("I am ${i} strange loops.");
+  forloop=  forloop.push("I am ${i} strange loops.");
   }
 }

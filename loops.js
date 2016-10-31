0d5630f790c97132f60840abd8889f0984e97927
jason brown <j.grant.brown@gmail.com>
2016-10-31 20:58:53 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,22 +1,23 @@
-/*var forLoop=[];
-
+/* var array=[];
+*/
+function forLoop(array)
  for (let i=0;i<=25;i++){
   if(i===1){
-  forLoop=[`I am ${i} strange loop.`];
+  forLoop.push(`I am ${i} strange loop.`);
   }
   else{
-  forLoop=[ ...forLoop,(`I am ${i} strange loops.`)];
+  forLoop.push(`I am ${i} strange loops.`);
   }
-}*/
-var array=[]
-function forLoop(array){
-  for (let i = 1;i < 25;i++){
-   if(i===1){
-array=[`I am ${i} strange loop.`];
+  return forLoop
 }
-   else{
-    array=[...array,(`I am ${i} strange loop.`)];}
-  }return array
 
+
+
+
+
+function whileLoop(number){
+  while(number>=0){
+    console.log(--number);}
+    return "done";
 
 }

275f91162017ed5d5690af43162134fd2596d242
jason brown <j.grant.brown@gmail.com>
2016-10-31 16:27:33 +0000

    Done.

--- loops.js
+++ loops.js
@@ -13,8 +13,8 @@ function forLoop(array){
   for (let i=0;i<=25;i++){
    if(i===1){
   array=[`I am ${i} strange loop.`];
-   }
+   
    else{
-   array+=[...array,(`I am ${i} strange loop.`)];
+   array=[...array,(`I am ${i} strange loop.`)];
    }
-}}}
+}}

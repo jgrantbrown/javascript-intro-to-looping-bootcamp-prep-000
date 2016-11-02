347dff5d61908367841badc5ec90b4d363fd78a1
jason brown <j.grant.brown@gmail.com>
2016-11-02 23:46:49 +0000

    Done.

--- loops.js
+++ loops.js
@@ -18,11 +18,13 @@ function whileLoop(number){
 
 }
 
-
+function maybeTrue() {
+    return Math.random() >= 0.5
+}
 
 function doWhileLoop(array)
 {
-do{array.shift()
-}while(function maybeTrue() {
-  return array.length> 0 && maybeTrue());
-}}
+  do{
+    array.shift()
+  } while(array.length > 0 && maybeTrue());
+}
